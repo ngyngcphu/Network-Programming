3d526885db15eb14f79e4682510fb9bde0f777b8
ngyngcphu <nguyenngocphu225@gmail.com>
2023-10-18 19:14:15 +0700

    feat(TCP-Socket): implement simple message exchange by TCP socket

---
+++ TCP-socket-simple/TCPServer.py
@@ -0,0 +1,14 @@
+from socket import *
+
+serverPort = 3000
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print('The server is ready to receive')
+
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+    capitalizeSentence = sentence.upper()
+    connectionSocket.send(capitalizeSentence.encode())
+    connectionSocket.close()
